4eccaa76d2910270f5f086520983c814b4b49ce1
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-03 17:35:18 -0400

    Add Game object variables

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,5 +19,11 @@ end
 
 # For the game that houses the board
 class Game < Board
-  # Placeholder
+  attr_accessor :player_one, :player_two
+
+  def initialize(spots, board, one, two)
+    super(spots, board)
+    @player_one = one
+    @player_two = two
+  end
 end
