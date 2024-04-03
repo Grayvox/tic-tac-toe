51488c1afee6cdf08838dad6662335cbb7ec0217
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-03 10:52:00 -0400

    Add board values

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,10 +3,21 @@
 require 'pry'
 require 'pry-byebug'
 
+# For the physical board itself
 class Board
-  # Placeholder
+  def initialize
+    @spots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @board = "
+    #{@spots[0]} | #{@spots[1]} | #{@spots[2]}
+    ---------
+    #{@spots[3]} | #{@spots[4]} | #{@spots[5]}
+    ---------
+    #{@spots[6]} | #{@spots[7]} | #{@spots[8]}
+    "
+  end
 end
 
+# For the game that houses the board
 class Game < Board
   # Placeholder
 end
