7b68e92aadebc3dcaa1fd8d508b5ca228b44d046
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-04 13:03:00 -0400

    Add Player object

--- lib/game.rb
+++ lib/game.rb
@@ -4,5 +4,5 @@ require_relative 'board'
 
 # For the game that houses the board
 class Game < Board
-  attr_accessor :player_one, :player_two
+  
 end
---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Defines the players themselves
+class Player
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
