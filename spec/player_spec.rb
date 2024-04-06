1962b75578c65bfc89b284d9311dd5b3b01fdb95
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-06 17:29:47 -0400

    Add Player test spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,17 @@
 # To be filled
+require 'spec_helper'
+require './lib/player'
+
+RSpec.describe 'TTT_Player' do
+  context 'when asked for the name of a player' do
+    it 'returns the correct output' do
+      expect(Player.new('Steve', 'O').name).to eq('Steve')
+    end
+  end
+
+  context 'when asked for the symbol assigned to a player' do
+    it 'returns the correct output' do
+      expect(Player.new('Steve', 'O').symbol).to eq('O')
+    end
+  end
+end
